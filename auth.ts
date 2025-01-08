3e3b367615043cdbcc731e2f8f1744261b26f24a
Cauê Favarim <cauebrolesef@gmail.com>
2025-01-08 20:21:29 -0300

    establish relationship between user and created blog

--- auth.ts
+++ auth.ts
@@ -1,29 +1,22 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import prisma from "./lib/db";
+import { createUser, getUser } from "./lib/actions";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
     async signIn({ user }) {
       try {
-        const existingUser = await prisma.user.findFirst({
-          where: { email: user.email! },
-        });
+        const existingUser = await getUser(user.email!);
 
         if (!existingUser) {
-          await prisma.user.create({
-            data: {
-              email: user.email!,
-              name: user.name!,
-              image: user.image!,
-            },
-          });
+          await createUser(user.name!, user.email!, user.image!);
         }
-        return true; 
+
+        return true;
       } catch (error) {
         console.error("Error signing in:", error);
-        return false; 
+        return false;
       }
     },
   },
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { auth } from "@/auth";
 import prisma from "@/lib/db";
 import { revalidatePath } from "next/cache";
 
@@ -11,6 +12,20 @@ export type State = {
   };
   message?: string | null;
 };
+
+export async function getUser(email: string) {
+  return await prisma.user.findFirst({ where: { email } });
+}
+
+export async function createUser(email: string, name: string, image: string) {
+  return await prisma.user.create({
+    data: {
+      email,
+      name,
+      image,
+    },
+  });
+}
 
 export async function getBlogs() {
   return await prisma.blog.findMany({
@@ -47,6 +62,13 @@ export async function createBlog({
     throw new Error("All fields are required.");
   }
 
+  const session = await auth();
+  if (!session) {
+    throw new Error("You must be signed in to create a blog.");
+  }
+  const email = session?.user?.email!;
+  const user = await getUser(email);
+
   try {
     await prisma.blog.create({
       data: {
@@ -57,7 +79,7 @@ export async function createBlog({
         categories: {
           connect: categoryIds.map((id) => ({ id })),
         },
-        creator_id: 1, // Replace with the actual user ID
+        creator_id: user!.id,
       },
     });
 
